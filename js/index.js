693b5554f8d425c4cd58652f47ab65e65900ec60
misinformation <kytsuna@gmail.com>
2019-11-14 17:22:05 +0000

    trained more at home

--- js/index.js
+++ js/index.js
@@ -6,8 +6,18 @@ if (Math.random() > 0.5) {
 
 const sect = document.querySelector('section');
 const para = document.createElement('p');
-para.textContent = 'We hope you enjoyed the ride.';
+para.textContent = 'We hope you enjoyed the pride.';
 sect.appendChild(para);
+
+
+/*  // цикл, щоб зробити 10 таких параграфів
+const sect = document.querySelector('section');
+for (let i = 0; i < 10; i++) {
+     const para = document.createElement('p');
+     para.textContent = 'We hope you enjoyed the pride.';
+     sect.appendChild(para);
+}
+*/
 
 const text = document.createTextNode(' — the premier source for web development knowledge.');
 const linkPara = document.querySelector('p');
@@ -15,39 +25,42 @@ linkPara.appendChild(text);
 
 sect.appendChild(linkPara.cloneNode(true));
 
-// old
-// linkPara.parentNode.removeChild(linkPara);
-
+//old
+//linkPara.parentNode.removeChild(linkPara);
+//new
 //linkPara.remove();
 
+para.style.color = 'white';
+para.style.backgroundColor = 'black';
+para.style.padding = '10px';
+para.style.width = '250px';
+para.style.textAlign = 'center';
+
+para.classList.add('selected');
 
 const clock = document.querySelector('.clock');
-setInterval(() => {
+setInterval (() => {
     clock.innerHTML = new Date().toLocaleTimeString();
 }, 1000);
 
-function clickButton() {
-    const para = document.createElement('p');
-    para.textContent = 'We hope you enjoyed the ride.';
-    sect.appendChild(para);
+document.querySelector('.click-me').addEventListener('click', clickButton)
+
+function clickButton(event) {
+     const para = document.createElement('p');
+     para.textContent = 'We hope you enjoyed the pride.';
+     sect.appendChild(para);
 }
+
+//e.preventDefault();
 
 $('.click-jq').click(() => {
-    $('p').toggle(1000);
+  $('p').toggle(1000);
 });
 
-const display = document.querySelector('.calc .display');
-document.querySelectorAll('.digits button, .opers button')
-    .forEach( button => button.addEventListener('click', digitOperPressed));
 
-function digitOperPressed(event) {
-    const btnText = event.target.innerText;
-    display.value += btnText;
-}
-
-const display = document.querySelector('.calc .display');
+const display = document.querySelector('.calc .display')
 document.querySelectorAll('.calc .digits button, .calc .opers button')
-    .forEach( button => button.addEventListener('click', digitOperPressed));
+   .forEach( button => button.addEventListener('click', digitOperPressed));
 
 function digitOperPressed(event) {
     const btnText = event.target.innerText;
@@ -60,3 +73,8 @@ document.querySelector('.calc .eq')
 function eqPressed() {
     display.value = eval(display.value)
 }
+
+//додоти цифри і операції
+//ділити на нуль не можна
+//можна декілька операцій підряд
+//додати память
